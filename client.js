5e766363a5869b5a12ba0dc22cf6326da0412615
pitmark <info@pitmark.com>
2016-12-07 14:37:16 +0900

    second commit

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 var nowdir = location.pathname;
 var nowdirs = nowdir.split('/');
@@ -12,4 +13,37 @@ class App extends React.Component {
   }
 }
 render(<App/>, document.getElementById('content_' + nowdirs[1] ));
+
+
+class Header extends React.Component {
+
+  handleSelect(eventKey){
+    console.log(eventKey);
+  }
+
+render() {
+
+    return (
+
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap Sample</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+
+        <Nav pullRight>
+          <NavDropdown id="my-dropdown" eventKey={1} title="Settings" >
+            <MenuItem eventKey={1}>About</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={2}>Sign out</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+
+    );
+}
+}
+
+render(<Header />, document.getElementById('header'));
 
